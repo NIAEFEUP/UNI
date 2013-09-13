c56c376cc3cc1c0839fe0c93d1998548fabe6b9f
Misirlou <diogotbasto@gmail.com>
2013-09-13 12:50:35 +0100

    mobile: js-adicionar classe da carta e teste para ver as cartas

--- mobile_client/game.js
+++ mobile_client/game.js
@@ -3,6 +3,39 @@ var joinrndgameurl="";
 var playurl="";
 //texto: [0-9]/<=> reverse/Stop/+2/+4/Wild
 
+function Card(color,value) {
+	
+	this.color = color;
+	this.value = value;
+	switch(color)
+	{
+		case 0:colortext="Red";break;
+		case 1:colortext="Green";break;
+		case 2:colortext="Blue";break;
+		case 3:colortext="Yellow";break;
+		case 4:colortext:"Wild";break;
+		default;colortext="Error";
+	}
+	switch(value)
+	{
+		case 10:this.valuetext="<i class='icon-blocked'></i>";break;
+		case 11:this.valuetext="+2";break;
+		case 12:this.valuetext="<i class='icon-loop'></i>";break;
+		case 13:this.valuetext="Wild";break;
+		case 14:this.valuetext="+4";break;
+		default:this.valuetext=value;
+	}
+}
+
+Card.prototype.html = function() {
+	return 	'<div class="card ">' +
+			'<div class='+this.colortext+'>' +
+			'<div class="cardtext">' +
+			this.valuetext +
+			'</div>' +
+			'</div>' +
+			'</div>';
+}
 
 
 $(document).ready(function() {
@@ -33,7 +66,7 @@ $(document).ready(function() {
 		
 		$.post(joinrndgameurl,{/*args*/},function(data){
 				//console.log(data);
-				if (data=="null")//json de jogo cheiro
+				if (data=="null")//json de jogo cheio
 				{
 					$('#lobbyerrormsg').text("Jogo cheio. Por favor tente outro jogo.");
 					$('#lobbyerrormsg').show();
@@ -53,7 +86,7 @@ $(document).ready(function() {
 		
 		$.post(playurl,{/*args*/},function(data){
 				//console.log(data);
-				if (data=="null")//json de jogo cheiro
+				if (data=="null")//json de jogo cheio
 				{
 
 				}else{
@@ -65,5 +98,19 @@ $(document).ready(function() {
 		});
 	});
 	
+	/*teste*/
+	$("#lobby").hide();
+	$("#game").show();
+	var c1=new Card("0","10");
+	var c2=new Card("1","3");
+	var c3=new Card("2","11");
+	var c4=new Card("3","12");
+	var c5=new Card("4","14");
+	
+	$("#game").append(c1.html());
+	$("#game").append(c2.html());
+	$("#game").append(c3.html());
+	$("#game").append(c4.html());
+	$("#game").append(c5.html());
 });
 
